e2c858f1159950a0499c71eac5030f4e7cc7dc42
ablcy <ablcy@users.noreply.github.com>
2026-05-20 01:54:48 +0800

    优化登录注册界面

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tell-v5.9.60';
+const CACHE_NAME = 'tell-v5.9.61';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
@@ -13,7 +13,7 @@ const STATIC_ASSETS = [
   '/admin.js'
 ];
 
-const API_CACHE_NAME = 'tell-api-v5.9.60';
+const API_CACHE_NAME = 'tell-api-v5.9.61';
 
 let notificationSettings = {
   floatingNotification: true,
--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 const VERSION = {
     major: 5,
     minor: 9,
-    patch: 60,
-    build: '202605200050',
+    patch: 61,
+    build: '202605200105',
     full: function() {
         return `v${this.major}.${this.minor}.${this.patch}`;
     },
